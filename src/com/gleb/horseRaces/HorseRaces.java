247092454cbdd10fd1b26c17300f24e6fa6d5f0d
Gleb Belkin <gleb.belkin@outlook.com>
2015-05-23 15:26:30 +0300

    Further development

--- src/com/gleb/horseRaces/HorseRaces.java
+++ src/com/gleb/horseRaces/HorseRaces.java
@@ -17,30 +17,56 @@ public class HorseRaces {
     private static boolean weHaveWinner = false;
     private static final int totalDistance = 70;
     public static Scanner userInput = new Scanner(System.in);
-
-    private static final int selectedHorse = 5;
+    private static int currentTimeMinutes = 0;
+    private static final float timeQuantMinutes = 5;
+    private static Horse winner;
 
     public static void main(String[] args) {
         createHorses();
         displayHorses();
-        int bet = userInput.nextInt();
-        System.out.println(bet);
-        /*
+
+        boolean correctBet = false;
+        int betIndex = 0;
+        while (!correctBet)
+        {
+            betIndex = userInput.nextInt();
+            if (betIndex < 0 || betIndex > horsesNumber){
+                System.out.println("Your bet is out of range, please select another one.");
+            } else {
+                correctBet = true;
+            }
+        }
+        //vector is zero-based
+        betIndex--;
+        Horse bet = stable.get(betIndex);
+        System.out.println("All bets are off!\n");
         while (!weHaveWinner) {
-            for (Iterator<Horse> iterator = stable.iterator(); iterator.hasNext(); ) {
-                Horse next = iterator.next();
-                next.addTime(1);
-                if (next.getDistanceCovered() > totalDistance) {
+            Iterator<Horse> itr = stable.iterator();
+            currentTimeMinutes += timeQuantMinutes;
+            System.out.printf("-------time: %1$s\n", currentTimeMinutes);
+            while (itr.hasNext()) {
+                Horse currentHorse = itr.next();
+                currentHorse.addDistanceByMinutes(timeQuantMinutes);
+                System.out.printf("\t %3$s %1$s: %2$s km\n", currentHorse.getName(), currentHorse.getDistanceCovered(), currentHorse.getNumber());
+                if (currentHorse.getDistanceCovered() > totalDistance) {
+                    winner = currentHorse;
                     weHaveWinner = true;
-                    break;
+                    //uncomment 'break' to avoid redundant calculations, commented for the all-participants output
+                    //break;
                 }
             }
-        }*/
+        }
+        System.out.printf("\nWinner: %1$s (N%2$s), your bet is %3$s (%4$s)\n", winner.getName(), winner.getNumber(), bet.getName(), bet.getNumber());
+        if (winner.getNumber() == bet.getNumber()) {
+            System.out.println("Congratulations, you win!");
+        } else {
+            System.out.println("Sorry man, you lose. Maybe next time...");
+        }
     }
 
     private static void displayHorses() {
         for (int i = 0; i < stable.size(); i++) {
-            System.out.printf("%2$s. %1$s\n", stable.get(i).getName(), i);
+            System.out.printf("%2$s. %1$s\n\t min speed: [%3$s kmph]\n\t max speed: [%4$s kmph]\n", stable.get(i).getName(), stable.get(i).getNumber(), stable.get(i).getMinSpeed(), stable.get(i).getMaxSpeed());
         }
 
     }
@@ -48,7 +74,9 @@ public class HorseRaces {
 
     private static void createHorses() {
         for (int i = 0; i < horsesNumber; i++) {
-            stable.add(new Horse(HorseName.randomName()));
+            Horse horse = new Horse(HorseName.randomName());
+            horse.giveNumber(i + 1);
+            stable.add(horse);
         }
     }
 }
--- src/com/gleb/horseRaces/assets/Horse.java
+++ src/com/gleb/horseRaces/assets/Horse.java
@@ -6,29 +6,55 @@ import java.util.Random;
  * Created by Gleb Belkin (gleb.belkin@outlook.com) on 20.05.2015.
  */
 public class Horse {
-    private final int averageSpeed = 10;
+    private final int averageSpeed = 60;
     private final String name;
-    private int minSpeed;
-    private int maxSpeed;
-    private int timeCompleted =0;
+
+
+    private final int minSpeed;
+    private final int maxSpeed;
+    private float timeCompletedHours = 0;
+    private static final Random rnd = new Random();
+    private float distanceCovered = 0;
+    private int number;
 
     public Horse(String name) {
         this.name = name;
-        Random rnd = new Random();
         minSpeed = averageSpeed - rnd.nextInt(5);
         maxSpeed = averageSpeed + rnd.nextInt(5);
     }
 
-    public void addTime(int hours){
-        timeCompleted+=hours;
+    public void addDistanceByHours(float hours) {
+        timeCompletedHours += hours;
+        int lapSpeed = minSpeed + (maxSpeed - minSpeed) * rnd.nextInt(10) / 10;
+        distanceCovered = distanceCovered + lapSpeed * hours;
     }
 
-    public int getDistanceCovered(){
-        return timeCompleted*minSpeed;
+    public void addDistanceByMinutes(float minutes) {
+        addDistanceByHours(minutes / 60);
+    }
+
+    public float getDistanceCovered() {
+        return distanceCovered;
     }
 
 
     public String getName() {
         return name;
+    }
+
+    public int getMinSpeed() {
+        return minSpeed;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void giveNumber(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
     }
 }
